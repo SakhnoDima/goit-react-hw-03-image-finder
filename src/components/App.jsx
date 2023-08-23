ae07e761c854f1127f8b3b3ede3ae863daa1ed6b
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-08-23 12:03:07 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
-export const App = () => {
-  return <div>SakhnoDima</div>;
-};
+import { Component } from 'react';
+import { Searchbar } from './index';
+import './styles/styles.css';
+
+export class App extends Component {
+  render() {
+    return <Searchbar />;
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import './Searchbar.css';
+
+export class Searchbar extends Component {
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm">
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
